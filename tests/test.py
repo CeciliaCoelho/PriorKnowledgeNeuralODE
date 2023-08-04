5b26e8c583c1cec0558ea3f2b4185084f764042d
CeciliaCoelho <ceciliaeduarda@live.com.pt>
2023-08-04 19:02:59 +0100

    test

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,8 @@
 import unittest
 from PriorKnowledgeNeuralODE import adaptiveFuncs
 
-class test(unittest.TestCase):
-    def Test(self):
+class Test(unittest.TestCase):
+    def test(self):
         print(1)
 
 unittest.main()
