0859e945fd1befdd6b6b27d0fd232dd4cb9928ca
CeciliaCoelho <ceciliaeduarda@live.com.pt>
2023-09-07 16:16:54 +0100

    add readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PriorKnowledgeNeuralODE',
-    version='0.1.0',
+    version='0.1.1',
     description='a compilation of methods to incorporate constraints into Neural ODE models',
     license='MIT',
     packages=['PriorKnowledgeNeuralODE'],
